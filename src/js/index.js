a54e9e34cf2a4dda0fdefe9bc53f188c8e603ac7
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2023-10-21 18:58:15 +0300

    20231021_finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import mobileNav from './modules/mobile-nav.js'
 mobileNav()
 
+import navOpenclick from './modules/navOpen.js'
+navOpenclick()
+
 import swiper from './modules/advant_slider.js'
 swiper
